293e23c91e9c8ebc5f0c7e0563ebca4cb87b868e
amonos <monos.adam@gmail.com>
2014-08-15 18:30:10 +0200

    Fixed UTF-8 encoding issues on Windows

--- subtitler.py
+++ subtitler.py
@@ -157,7 +157,7 @@ class Subtitler:
                 data = input_sub.read()
 
         print("Encoding subtitle {:s} to UTF-8".format(subtitle))
-        with open(temp, 'w+') as output:
+        with open(temp, 'w+', encoding='UTF-8') as output:
             # Removes BOM from UTF-8 file
             if data.startswith(u'\ufeff'):
                 data = data[1:]
