1f37516c2b9f68f99100a965c48085280000995e
amonos <amonos@alvicom.hu>
2014-08-15 13:58:57 +0200

    Commented BOM removal

--- subtitler.py
+++ subtitler.py
@@ -82,6 +82,7 @@ class Subtitler:
 
         print("Encoding subtitle {:s} to UTF-8".format(subtitle))
         with open(temp, 'w+') as output:
+            # Removes BOM from UTF-8 file
             if data.startswith(u'\ufeff'):
                 data = data[1:]
             output.write(data)
