be65870ad1bf02dce5fce93661d12d41980b7fb3
amonos <amonos@alvicom.hu>
2017-08-28 14:54:16 +0200

    cx freeze setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from cx_Freeze import setup, Executable
+
+base = None
+
+executables = [Executable('subtitler.py', base=base)]
+
+options = {
+    'build_exe': {
+    }
+}
+
+setup(name='subtitler', version='1.0', description='OpenSubtitles downloader', executables=executables, requires=['core'])
