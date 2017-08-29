d9053d692bc789c6e778a5802898359c933b4423
amonos <amonos@alvicom.hu>
2017-08-29 14:27:20 +0200

    cx freeze setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from cx_Freeze import setup, Executable
 
+
+include_files = ['subtitler.conf']
 base = None
 
 executables = [Executable('subtitler.py', base=base)]
 
 options = {
     'build_exe': {
+        'include_files': include_files
     }
 }
 
