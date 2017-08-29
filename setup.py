80d96be7263ac3ff1a4cfbd22310020115afbd93
amonos <amonos@alvicom.hu>
2017-08-29 14:37:41 +0200

    cx freeze data file revert

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from cx_Freeze import setup, Executable
 
 
-include_files = ['subtitler.conf']
 base = None
 
 executables = [Executable('subtitler.py', base=base)]
 
 options = {
     'build_exe': {
-        'include_files': include_files
     }
 }
 
